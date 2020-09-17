0046494fa800f9fd99a8b46599d14755b55705e4
MaximKonovodov <ravenheart.sp@gmail.com>
2020-09-17 10:52:35 +0300

    refactoring

--- src/pages/Profile/components/BookCollection.js
+++ src/pages/Profile/components/BookCollection.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-const BooksCollection = () => {
+const BooksCollection = ({ books }) => {
+  if (!books) return <div>Loading</div>;
   return (
     <div
       className='tab-pane fade'
@@ -9,125 +13,26 @@ const BooksCollection = () => {
       aria-labelledby='mes-tab'
     >
       <hr />
-      <form className='form' action='##' method='post' id='registrationForm'>
-        <div className='form-group'>
-          <div className='col-xs-6'>
-            <label htmlFor='first_name'>
-              <h4>First name</h4>
-            </label>
-            <input
-              type='text'
-              className='form-control'
-              name='first_name'
-              id='first_name'
-              placeholder='first name'
-              title='enter your first name if any.'
-            />
-          </div>
-        </div>
-        <div className='form-group'>
-          <div className='col-xs-6'>
-            <label htmlFor='last_name'>
-              <h4>Last name</h4>
-            </label>
-            <input
-              type='text'
-              className='form-control'
-              name='last_name'
-              id='last_name'
-              placeholder='last name'
-              title='enter your last name if any.'
-            />
-          </div>
-        </div>
 
-        <div className='form-group'>
-          <div className='col-xs-6'>
-            <label htmlFor='phone'>
-              <h4>Phone</h4>
-            </label>
-            <input
-              type='text'
-              className='form-control'
-              name='phone'
-              id='phone'
-              placeholder='enter phone'
-              title='enter your phone number if any.'
-            />
-          </div>
-        </div>
-
-        <div className='form-group'>
-          <div className='col-xs-6'>
-            <label htmlFor='email'>
-              <h4>Email</h4>
-            </label>
-            <input
-              type='email'
-              className='form-control'
-              name='email'
-              id='email'
-              placeholder='you@email.com'
-              title='enter your email.'
-            />
-          </div>
-        </div>
-        <div className='form-group'>
-          <div className='col-xs-6'>
-            <label htmlFor='email'>
-              <h4>Location</h4>
-            </label>
-            <input
-              type='email'
-              className='form-control'
-              id='location'
-              placeholder='somewhere'
-              title='enter a location'
-            />
-          </div>
-        </div>
-        <div className='form-group'>
-          <div className='col-xs-6'>
-            <label htmlFor='password'>
-              <h4>Password</h4>
-            </label>
-            <input
-              type='password'
-              className='form-control'
-              name='password'
-              id='password'
-              placeholder='password'
-              title='enter your password.'
-            />
-          </div>
-        </div>
-        <div className='form-group'>
-          <div className='col-xs-6'>
-            <label htmlFor='password2'>
-              <h4>Verify</h4>
-            </label>
-            <input
-              type='password'
-              className='form-control'
-              name='password2'
-              id='password2'
-              placeholder='password2'
-              title='enter your password2.'
-            />
-          </div>
-        </div>
-        <div className='form-group'>
-          <div className='col-xs-12'>
-            <br />
-            <button className='btn btn-lg btn-success' type='submit'>
-              <i className='glyphicon glyphicon-ok-sign'></i> Save
-            </button>
-            <button className='btn btn-lg' type='reset'>
-              <i className='glyphicon glyphicon-repeat'></i> Reset
-            </button>
-          </div>
-        </div>
-      </form>
+      <Accordion>
+        {books.map((book) => (
+          <Card key={book.id}>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant='link' eventKey={book.id}>
+                {book.name}
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={book.id}>
+              <Card.Body>
+                <p>Название: {book.name}</p>
+                <p>Описание: {book.description}</p>
+                <p>Автор: {book.author.name}</p>
+                <p>Жанр: {book.category.value}</p>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        ))}
+      </Accordion>
     </div>
   );
 };
